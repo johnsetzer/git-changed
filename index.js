f0704668898bd995ae26113910732875b005bcf8
John Setzer <jsetzer@yammer-inc.com>
2014-09-09 00:02:22 -0700

    Switched changedFiles() from byline to es.split

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var exec = require('child_process').exec;
 var spawn = require('child_process').spawn;
 var fs = require('fs');
-var byline = require('byline');
+var es = require('event-stream');
 
 // Callbacks last commit
 exports.lastCommit = function (cb) {
@@ -38,21 +38,19 @@ exports.changedFiles = function (range, cb) {
 	var git = spawn('git', ['diff', range, '--name-status']);
 	var changedFiles = [];
 
-	var lineStream = byline.createStream(git.stdout);
-
-	lineStream.on('data', function(line) {
-		line = line.toString();
-	  changedFiles.push({
-	  	change: line.slice(0,1),
-	  	file: line.slice(1).trim()
-	  });
-	});
-
-	lineStream.on('error', function(error) {
-	  cb(error);
-	});
-
-	lineStream.on('end', function() {
-	  cb(null, changedFiles);
-	});
+	git.stdout
+		.pipe(es.split())
+		.pipe(es.map(function (line, cb) {
+	    changedFiles.push({
+			  change: line.slice(0,1),
+	      file: line.slice(1).trim()
+	    });
+	    cb(null, line);
+	  }))
+	  .on('end', function() {
+		  cb(null, changedFiles);
+		})
+		.on('error', function(error) {
+		  cb(error);
+		});
 };
